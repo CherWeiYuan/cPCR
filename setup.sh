820621e6b7ddf4e424095ef3dc0670a72a5317d1
WY <47816698+CherWeiYuan@users.noreply.github.com>
2025-03-07 14:34:25 +0800

    Add sample input and output

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
-mamba create --name pcr \
-    blast biopython gtfparse spyder primer3 primer3-py pysam tqdm \
+# Create mamba environment
+mamba create --name cpcr \
+    blast biopython gtfparse rich-argparse spyder primer3 primer3-py pysam tqdm \
     -c bioconda -y
+
+# Install cPCR
+git clone https://github.com/CherWeiYuan/Panthera.Git
+pip install -e .
+
